d8753fb8ef87dd55e0f90bb69ed556f417e36c4a
brianlin12345 <brianlin12345@gamil.com>
2024-03-07 15:11:29 +0800

    script

--- MyEPA/Docs/UpdateScript/水質通報細項設定_20240307.sql
+++ MyEPA/Docs/UpdateScript/水質通報細項設定_20240307.sql
@@ -1,8 +1,17 @@
 --水質通報細項設定
 
 --Null設定
---O_Turbidity　　　　　原水濁度	
---Chlorine　　　　　　自由有效餘氯　檢驗值
---EColi　　　　　　　　大腸桿菌群　檢驗值
---Hydrogen　　　　　　氫離子濃度指數　檢驗值
---Turbidity　　　　　　濁度　檢驗值
+--[O_Turbidity]　　　　原水濁度	
+--[Chlorine]　　　　　　自由有效餘氯　檢驗值
+--[EColi]　　　　　　　　大腸桿菌群　檢驗值
+--[Hydrogen]　　　　　　氫離子濃度指數　檢驗值
+--[Turbidity]　　　　　　濁度　檢驗值
+
+--WaterCheckDetail
+alter TABLE WaterCheckDetail ALTER COLUMN [O_Turbidity] [decimal](18, 3) NULL
+alter TABLE WaterCheckDetail ALTER COLUMN [Chlorine] [decimal](18, 3) NULL
+alter TABLE WaterCheckDetail ALTER COLUMN [EColi] [decimal](18, 3) NULL
+alter TABLE WaterCheckDetail ALTER COLUMN [Hydrogen] [decimal](18, 3) NULL
+alter TABLE WaterCheckDetail ALTER COLUMN [Turbidity] [decimal](18, 3) NULL
+
+
