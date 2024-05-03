980f5dfa4e487e698057d5c492fae57ac7fe8294
brianlin12345 <brianlin12345@gamil.com>
2024-05-03 10:49:43 +0800

    資源調度演練 v1 Q 環境督察總隊

--- MyEPA/Controllers/Rec/RecResourceController.cs
+++ MyEPA/Controllers/Rec/RecResourceController.cs
@@ -57,22 +57,17 @@ namespace MyEPA.Controllers
             }
 
             IEnumerable<RecResourceModel> iquery = RecResourceService.GetByDiasterId(diasterId.Value);
-
-            //調度需求(改自己，全部看)
-            //提供需求(改自己，)
-            //var user = GetUserBrief();
-            //if (!user.IsAdmin)
-            //{
-            //    iquery = iquery.Where(a => a.CityId == user.CityId);
-            //}
-
+            
             ////iquery = iquery.Where(a => a.Type == type);
             iquery = iquery.OrderByDescending(a => a.Id);
             List<RecResourceModel> result = iquery.ToList();
 
+            var user = GetUserBrief();
+
             ViewBag.Citys = CityService.GetAll();
-            ViewBag.IsAdmin = GetIsAdmin();            
-            ViewBag.User = GetUserBrief();
+            ////環衛組：視為admin
+            ViewBag.IsAdmin = user.Town.Trim() == "環境督察總隊".Trim() || GetIsAdmin();
+            ViewBag.User = user;
 
             //querystring
             ViewBag.DiasterId = diasterId;
