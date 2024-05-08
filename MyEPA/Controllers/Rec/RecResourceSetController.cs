106f84162b4744d31c2d53f794e9d39910b3a369
brianlin12345 <brianlin12345@gamil.com>
2024-05-08 08:32:27 +0800

    pdf 調度審核 v1

--- MyEPA/Controllers/Rec/RecResourceSetController.cs
+++ MyEPA/Controllers/Rec/RecResourceSetController.cs
@@ -1,14 +1,19 @@
-﻿using DocumentFormat.OpenXml.Office2010.Excel;
+﻿using Microsoft.Office.Interop.Word;
 using MyEPA.Extensions;
 using MyEPA.Models;
 using MyEPA.Models.FilterParameter;
 using MyEPA.Repositories;
 using MyEPA.Services;
+using NPOI.OpenXmlFormats.Wordprocessing;
+using NPOI.XWPF.UserModel;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Document = Microsoft.Office.Interop.Word.Document;
 
 namespace MyEPA.Controllers.Rec
 {
@@ -157,6 +162,193 @@ namespace MyEPA.Controllers.Rec
             };
 
             return result;
+        }
+
+        public ActionResult DownPDF(int Id)
+        {
+            //我要下載的檔案位置
+            string filefolder = Server.MapPath("~/FileDatas/Template/");
+            string fileName = "應變資源調度審核情形.docx";
+
+            var datas = RecResourceService.Get(Id);
+
+            string path = filefolder + fileName;
+
+            //取得檔案名稱
+            string filename = System.IO.Path.GetFileName(path);
+
+            StringBuilder sb = new StringBuilder();
+            string toFolder = Server.MapPath("~/FileDatas/Temp/");
+            string toPdfName = fileName + DateTime.Now.ToString("yyyy-MM-dd") + ".pdf";
+            string toPdfPath = toFolder + toPdfName;
+
+            using (FileStream stream = System.IO.File.OpenRead(path))
+            {
+                XWPFDocument docx = new XWPFDocument(stream);
+
+                ////Dictionary<string, string> textDic = new Dictionary<string, string>()
+                ////    {
+                ////        {"City",  CityService.GetAll().Where(a => a.Id == datas.CityId).FirstOrDefault().City},
+                ////        {"CreateDate", datas.CreateDate.ToShortDateString() },
+                ////        {"ContactPerson", datas.ContactPerson },
+                ////        {"ContactMobilePhone", datas.ContactMobilePhone },
+                ////        {"Reason", datas.Reason }
+                ////};
+
+                ////foreach (XWPFTable table in docx.Tables)
+                ////{
+                ////    foreach (XWPFTableRow row in table.Rows)
+                ////    {
+                ////        foreach (XWPFTableCell cell in row.GetTableCells())     //遍歷每一行中的每一列
+                ////        {
+                ////            foreach (var paragraph in cell.Paragraphs)  // 遍歷當前表格里的所有（段落）段
+                ////            {
+                ////                foreach (var texts in textDic)
+                ////                {
+                ////                    try
+                ////                    {
+                ////                        var repStr = "[$" + texts.Key + "$]";
+                ////                        if (paragraph.Text.Contains(repStr))
+                ////                            paragraph.ReplaceText(repStr, texts.Value);  // 替换段落中的文字
+                ////                    }
+                ////                    catch (Exception ex)
+                ////                    {
+                ////                        // 不处理
+                ////                        continue;
+                ////                    }
+                ////                }
+                ////            }
+                ////        }
+                ////    }
+                ////}
+
+                //////讀取表格
+                ////if (1 == 1)
+                ////{
+                ////    List<RecResourceModel> sList = RecResourceService.GetByCityId(datas.CityId)
+                ////                                        .Where(a => a.Type == datas.Type)
+                ////                                        .OrderByDescending(a => a.Id)
+                ////                                        .ToList();
+
+                ////    XWPFTable table = docx.Tables[0];
+
+
+                ////    int refn = 0;
+                ////    if (datas.Type == 1)
+                ////    {
+                ////        //表格第5列(清單)
+                ////        refn = 4;
+                ////    }
+                ////    else if (datas.Type == 2)
+                ////    {
+                ////        //表格第4列(清單)
+                ////        refn = 3;
+                ////    }
+
+                ////    XWPFTableRow refRows = table.Rows[refn];
+
+                ////    //清單資料
+                ////    int count = 0;
+                ////    foreach (RecResourceModel s in sList)
+                ////    {
+                ////        Dictionary<int, string> sdic = new Dictionary<int, string>()
+                ////        {
+                ////            { 0, Code.GetRecItems().Where(a => a.Key == s.Items).FirstOrDefault().Value },
+                ////            { 1, s.Spec},
+                ////            { 2, s.Quantity.ToString()},
+                ////            { 3, s.Unit},
+                ////            { 4, s.USDate.ToShortDateString() + "~" + s.UEDate.ToShortDateString()}
+                ////        };
+
+                ////        CT_Row ctrow = refRows.GetCTRow();
+                ////        CT_Row targetRow = new CT_Row();
+
+                ////        int index = 0;
+                ////        foreach (CT_Tc item in ctrow.Items)
+                ////        {
+                ////            CT_Tc addTc = targetRow.AddNewTc();
+                ////            addTc.tcPr = item.tcPr;
+
+                ////            IList<CT_P> list_p = item.GetPList();
+
+                ////            foreach (var p in list_p)
+                ////            {
+                ////                CT_P addP = addTc.AddNewP();
+                ////                addP.pPr = p.pPr;//段落樣式
+                ////                IList<CT_R> list_r = p.GetRList();
+                ////                foreach (CT_R r in list_r)
+                ////                {
+                ////                    CT_R addR = addP.AddNewR();
+                ////                    addR.rPr = r.rPr;//run樣式，包含字體
+                ////                    List<CT_Text> list_text = r.GetTList();
+
+                ////                    //設定Text內容
+                ////                    string text = sdic[index];
+                ////                    for (int i = 0; i < text.Length; i++)
+                ////                    {
+                ////                        CT_Text addText = addR.AddNewT();
+                ////                        addText.Value = text.Substring(i, 1);
+                ////                    }
+
+                ////                    ////foreach (CT_Text text in list_text)
+                ////                    ////{
+                ////                    ////    CT_Text addText = addR.AddNewT();
+                ////                    ////    addText.space = text.space;
+                ////                    ////    addText.Value = text.Value;
+                ////                    ////}
+                ////                }
+
+                ////                index++;
+                ////            }
+                ////        }
+
+                ////        //新增資料行
+                ////        XWPFTableRow mrow = new XWPFTableRow(targetRow, table);
+                ////        table.AddRow(mrow);
+                ////    }
+
+                ////    //刪除字型保留列(xx)
+                ////    if (count == 0)
+                ////    {
+                ////        //refRows
+                ////        table.RemoveRow(refn);
+                ////    }
+                ////    count++;
+                ////}
+
+                if (!Directory.Exists(toFolder))
+                {
+                    Directory.CreateDirectory(toFolder);
+                }
+
+                string toFileName = fileName + DateTime.Now.ToString("yyyy-MM-dd") + ".docx";
+                string toPath = toFolder + toFileName;
+
+                FileStream xlsFile = new FileStream(toPath, FileMode.Create, FileAccess.Write);
+                docx.Write(xlsFile);
+                xlsFile.Close();
+                docx.Close();
+
+                //讀成串流
+                var tmpStream = new FileStream(toPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+                //回傳出檔案
+                return File(tmpStream, GetContentType("docx"), toFileName);
+
+                ////// 轉換成pdf
+                ////var app = new Microsoft.Office.Interop.Word.Application();
+                ////// 開啟 Word 文件
+                ////Document doc = app.Documents.Open(toPath);
+                ////// 轉換為 PDF
+                ////doc.ExportAsFixedFormat(toPdfPath, WdExportFormat.wdExportFormatPDF);
+                //////doc.Close();
+                ////((Microsoft.Office.Interop.Word._Document)doc).Close(false);
+                ////((Microsoft.Office.Interop.Word._Application)app).Quit(false);
+            }
+
+            //讀成串流
+            var iStream = new FileStream(toPdfPath, FileMode.Open, FileAccess.Read, FileShare.Read);
+            //回傳出檔案
+            return File(iStream, GetContentType("docx"), toPdfName);
         }
     }
 }
