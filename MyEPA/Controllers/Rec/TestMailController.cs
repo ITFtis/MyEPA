c951f9aea2edf62031ed83d037adcde1f14638d2
brianlin12345 <brianlin12345@gamil.com>
2024-05-13 08:24:56 +0800

    Email 測試 v1

---
+++ MyEPA/Controllers/Rec/TestMailController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEPA.Controllers.Rec
+{
+    public class TestMailController : LoginBaseController
+    {
+        // GET: TestMail
+        public ActionResult Index()
+        {
+            bool isAdmin = GetIsAdmin();
+
+            //限定管理者使用
+            if (!isAdmin)
+            {                
+                return RedirectToAction("LoginRedirect", "Home", new { });
+            }
+
+            return View();
+        }
+    }
+}
