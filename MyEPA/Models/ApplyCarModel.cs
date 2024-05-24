cd7a5a3eeef28ba889cd8a2e9489fabcbc6c37db
brianlin12345 <brianlin12345@gamil.com>
2024-05-24 08:59:56 +0800

    3.【請求支援】修正建議 有些環保局沒有審核清潔隊請求支援的按鈕，須檢視更新。 v2 車輛設備 建檔/確認 單位

--- MyEPA/Models/ApplyCarModel.cs
+++ MyEPA/Models/ApplyCarModel.cs
@@ -7,11 +7,6 @@ namespace MyEPA.Models
     {
         public List<ApplyCarDetailModel> Details = new List<ApplyCarDetailModel>();
 
-        /// <summary>
-        /// 建立者單位
-        /// </summary>
-        public string CreateUserDuty { get; set; }
-
         public void CleanDetails()
         {
             Details.Clear();
