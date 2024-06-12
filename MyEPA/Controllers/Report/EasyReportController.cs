0389c225b9e20e4583fff6e1694967704d22bbaa
brianlin12345 <brianlin12345@gamil.com>
2024-06-12 11:21:58 +0800

    緊急應變統計表 v1

---
+++ MyEPA/Controllers/Report/EasyReportController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEPA.Controllers.Report
+{
+    public class EasyReportController : LoginBaseController
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        // GET: EasyReport
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public ActionResult DisasterResponseReport()
+        {
+            if (!GetIsAdmin())
+                return RedirectToAction("LoginRedirect", "Home");
+
+            return View();
+        }
+    }
+}
