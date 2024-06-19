a3f0b0c48eb5d8744bd71588c304cbf3471470ef
brianlin12345 <brianlin12345@gamil.com>
2024-06-19 15:56:03 +0800

    script

--- MyEPA/Docs/UpdateScript/20240619_支援3項目確認時間.sql
+++ MyEPA/Docs/UpdateScript/20240619_支援3項目確認時間.sql
@@ -1,3 +1,5 @@
 --新增欄位([Users])
 alter TABLE [Users] Add UpdateUser Nvarchar(200) collate SQL_Latin1_General_CP1_CI_AS NULL
 
+--新增欄位(Disinfector)
+alter TABLE Disinfector Add UpdateUser Nvarchar(200) collate SQL_Latin1_General_CP1_CI_AS NULL
