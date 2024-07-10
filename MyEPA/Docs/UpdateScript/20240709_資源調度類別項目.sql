e1b157ed704997eba617a59555664a821619f5d2
brianlin12345 <brianlin12345@gamil.com>
2024-07-10 11:34:28 +0800

    script

--- MyEPA/Docs/UpdateScript/20240709_資源調度類別項目.sql
+++ MyEPA/Docs/UpdateScript/20240709_資源調度類別項目.sql
@@ -24,10 +24,10 @@ GO
 
 SET IDENTITY_INSERT [dbo].[DisinfectantType] ON 
 
-INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (1, N'A01', N'環境消毒(固態Kg)')
-INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (2, N'A02', N'環境消毒(液態L)')
-INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (3, N'A03', N'登革熱(固態Kg)')
-INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (4, N'A04', N'登革熱(液態L)')
+INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (1, N'A01', N'環境用藥(固態Kg)')
+INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (2, N'A02', N'環境用藥(液態L)')
+INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (3, N'A03', N'登革熱用藥(固態Kg)')
+INSERT [dbo].[DisinfectantType] ([Id], [Type], [Name]) VALUES (4, N'A04', N'登革熱用藥(液態L)')
 SET IDENTITY_INSERT [dbo].[DisinfectantType] OFF
 GO
 SET IDENTITY_INSERT [dbo].[DisinfectorType] ON 
