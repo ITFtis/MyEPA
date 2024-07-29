8040ba507594c12009ea9cf14132703799f20b55
brianlin12345 <brianlin12345@gamil.com>
2024-07-29 15:13:47 +0800

    密碼驗證 v2

--- MyEPA/_core/PwdHelper.cs
+++ MyEPA/_core/PwdHelper.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 
 namespace MyEPA
@@ -16,9 +17,29 @@ namespace MyEPA
         {
             ErrorMessage = "";
 
+            if (password == null)
+            {
+                ErrorMessage = "未提供密碼";
+                return false;
+            }
+
+            List<string> errors = new List<string>();
+
             if (string.IsNullOrWhiteSpace(password) || password.Length < 12)
             {
-                ErrorMessage = "抱歉，密碼長度須達 12 碼以上";
+                errors.Add("密碼長度須達 12 碼以上");
+            }
+
+            string patternEN = @"^(?=.*[a-zA-Z])(?=.*\d).+$";
+            Regex regEN = new Regex(patternEN);
+            if (!regEN.IsMatch(password))
+            {
+                errors.Add("密碼需包含1個英文及1個數字");
+            }
+
+            if (errors.Count > 0)
+            {
+                ErrorMessage = "抱歉：" + string.Join("、", errors);
                 return false;
             }
 
