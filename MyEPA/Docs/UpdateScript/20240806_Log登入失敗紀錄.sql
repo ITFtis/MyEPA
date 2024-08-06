e9f971e50f5e1d750a2301d21032d20d3d0fe3d7
brianlin12345 <brianlin12345@gamil.com>
2024-08-06 14:12:55 +0800

    script

--- MyEPA/Docs/UpdateScript/20240806_Log登入失敗紀錄.sql
+++ MyEPA/Docs/UpdateScript/20240806_Log登入失敗紀錄.sql
@@ -3,7 +3,7 @@
 
 --3個欄位(Type, PwdKeyin, SourceIP)
 --Type：1正常登入,2登入失敗
---IsOver：是否超過登入有效次數
+--IsOver：是否納入登入失敗次數(0否1是)
 alter Table UserLoginLog add [Type] [int] NULL
 alter Table UserLoginLog add [PwdKeyIn] [nvarchar](50) NULL
 alter Table UserLoginLog add [SourceIP] [varchar](100) NULL
@@ -11,5 +11,5 @@ alter Table UserLoginLog add [IsOver] [bit] NULL
 Go
 
 Update UserLoginLog
-Set Type = 1
+Set Type = 0
 Go
