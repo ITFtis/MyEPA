04a282daa8dd6cbb7bd9dfc3bfca93610eae83f8
brianlin12345 <brianlin12345@gamil.com>
2024-08-06 09:09:03 +0800

    script 2

--- MyEPA/Docs/UpdateScript/20240806_Log登入失敗紀錄.sql
+++ MyEPA/Docs/UpdateScript/20240806_Log登入失敗紀錄.sql
@@ -2,7 +2,14 @@
 --每個登入的紀錄 包括 日期時間 帳號輸入的密碼以及IP
 
 --3個欄位(Type, PwdKeyin, SourceIP)
---[Type] [int] NOT NULL,
---[PwdKeyin] [nvarchar](50) NULL,
---[SourceIP] [varchar](24) NOT NULL,
+--Type：1正常登入,2登入失敗
+--IsOver：是否超過登入有效次數
+alter Table UserLoginLog add [Type] [int] NULL
+alter Table UserLoginLog add [PwdKeyIn] [nvarchar](50) NULL
+alter Table UserLoginLog add [SourceIP] [varchar](100) NULL
+alter Table UserLoginLog add [IsOver] [bit] NULL
+Go
 
+Update UserLoginLog
+Set Type = 1
+Go
