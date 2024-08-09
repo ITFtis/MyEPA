f57f81125963995866d371109c6b415e1dfd985a
brianlin12345 <brianlin12345@gamil.com>
2024-08-09 13:19:38 +0800

    文字調整：頭份鎮改為頭份市

--- MyEPA/Docs/UpdateScript/20240809_頭份鎮_頭份市.sql
+++ MyEPA/Docs/UpdateScript/20240809_頭份鎮_頭份市.sql
@@ -1,1 +1,12 @@
-Select Town, '繷カ' From [Users] Where Town = '繷马'
+--Select Town, '繷カ' From [Users] Where Town = '繷马'
+--Select Name, '繷カ' From Town Where Name = '繷马'
+
+--1.眀腹
+Update [Users]
+Set Town = '繷カ' 
+Where Town = '繷马'
+
+--2.秏马絏
+Update Town
+Set Name = '繷カ' 
+Where Name = '繷马'
