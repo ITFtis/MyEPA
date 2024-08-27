f0d1e3db40fd389660f1a7e3f5e3329d6698657b
brianlin12345 <brianlin12345@gamil.com>
2024-08-27 14:36:46 +0800

    script

---
+++ MyEPA/Docs/UpdateScript/20240827_單位轉換.sql
@@ -0,0 +1,22 @@
+--1.資料更新(存中文)
+--Select　Distinct Duty From [Users]
+Update xxxxx Set oooo = ooooo Where xxxxx = ooooooo
+
+--Select Distinct Town, Len(Town) From [Users]　Order By Len(Town) DESC
+
+--Select Distinct Name, Len(Name)  From [Department]　Order By Len(Name) DESC
+
+--Select Distinct Name, Len(Name) From [ContactManualDepartment] Order By Len(Name) DESC
+
+--Select * From [Area]
+
+--Select Distinct COUNTY_NAME, Len(COUNTY_NAME) From [DRINK_DETAIL] Order By Len(COUNTY_NAME) DESC
+
+--Select Distinct COUNTY_NAME, Len(COUNTY_NAME) From [DRINK_SECOND]　Order By Len(COUNTY_NAME) DESC
+
+--Select Distinct Town, Len(Town) From [View_Users_Position] Order By Len(Town) DESC　
+
+--Select Distinct Town, Len(Town) From [View_ContactManul]　Order By Len(Town) DESC　
+
+----------------------------------------------
+--2.代碼更新
