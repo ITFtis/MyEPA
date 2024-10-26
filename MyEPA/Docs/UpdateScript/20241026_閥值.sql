bdfc69492f9f976169f0554a4cd31ba8b19bb896
brianlin12345 <brianlin12345@gamil.com>
2024-10-26 14:29:24 +0800

    script 2

--- MyEPA/Docs/UpdateScript/20241026_閥值.sql
+++ MyEPA/Docs/UpdateScript/20241026_閥值.sql
@@ -6,6 +6,9 @@
 
 
 ------------------------------------------------------------------------
+
+
+
 ------閥值(消毒設備)
 ------DiasterId int Not Null,  CtPoint int NULL, LogBDate [datetime] NULL, [LogUser] [nvarchar](200) NULL,
 ----Select * From LogDisinfector Order By Id Desc
@@ -13,3 +16,26 @@
 ------閥值(藥品)
 ------DiasterId int Not Null,  CtPoint int NULL, LogBDate [datetime] NULL, [LogUser] [nvarchar](200) NULL,
 ----Select * From LogDisinfectant Order By Id Desc
+
+--閥值(消毒設備)
+----DB：epaemis_local_20240930_t
+--Select 1 AS Id, 999999999 AS DiasterId, 
+--       City, Town, ContactUnit, DisinfectInstrument, Standard, Amount, ROCyear, UpdateTime, ConfirmTime, UseType, UpdateUser,
+--	   0 AS CtPoint, GetDate() AS LogBDate, 'system' AS LogUser
+--From epaemis_local_20240930_t.dbo.Disinfector 
+
+--閥值(藥品)
+----預測資料
+--Select 1 AS Id, 999999999 AS DiasterId,
+--       City, Town, ContactUnit, DrugName, DrugType, DrugState, Amount, Density, Area, ServiceLife, UpdateTime, ConfirmTime, UseType, ActiveIngredients1, ActiveIngredients2, UpdateUser,
+--	   0 AS CtPoint, GetDate() AS LogBDate, 'system' AS LogUser
+--From epaemis_local_20240930_t.dbo.Disinfectant 
+
+
+Update LogDisinfector
+Set CtPoint = Convert(float, ISNULL(Amount, 0)) / 2 
+Where DiasterId = 999999999
+
+Update LogDisinfectant
+Set CtPoint = Convert(float, ISNULL(Amount, 0)) / 2 
+Where DiasterId = 999999999
