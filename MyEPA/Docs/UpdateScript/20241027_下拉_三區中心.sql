51e1a1080b741e0327e26391eade25a07130c851
brianlin12345 <brianlin12345@gamil.com>
2024-10-28 15:52:56 +0800

    script

--- MyEPA/Docs/UpdateScript/20241027_下拉_三區中心.sql
+++ MyEPA/Docs/UpdateScript/20241027_下拉_三區中心.sql
@@ -3,3 +3,17 @@ Insert Into Duty(Id, Name)
 Select 5 AS Id, '三區中心' AS Name
 
 
+
+--縣市
+Insert Into City(City, AreaId, IsCounty, WaterDivisionId, Sort, type)
+select '三區中心' AS City, 1 AS AreaId, 0 AS IsCounty, 0 AS WaterDivisionId, 1 AS Sort, 3 AS type 
+Go
+
+--鄉鎮
+Declare @CityId AS Int
+Select @CityId = Id from City Where City = '三區中心'
+
+Insert Into Town(CityId, Name, IsTown)
+select @CityId AS CityId, '三區中心' AS Name, 0 AS IsTown
+Go
+
