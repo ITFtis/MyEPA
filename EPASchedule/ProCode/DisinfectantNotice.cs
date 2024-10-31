745c1b46a1db5a198b335bd2a406904a176a7981
brianlin12345 <brianlin12345@gamil.com>
2024-10-31 17:25:04 +0800

    v2

--- EPASchedule/ProCode/DisinfectantNotice.cs
+++ EPASchedule/ProCode/DisinfectantNotice.cs
@@ -41,11 +41,11 @@ namespace EPASchedule
             {
                 DisinfectantService DisinfectantService = new DisinfectantService();
 
-                //母體資料
-                var datas = DisinfectantService.GetAll();
+                //所有藥劑
+                var ants = DisinfectantService.GetAll();
 
                 //City, Town, ContactUnit, DrugName
-                var tmp = datas.Select(a => new
+                var tmp = ants.Select(a => new
                 {
                     City = a.City,
                     Town = a.Town,
@@ -55,7 +55,39 @@ namespace EPASchedule
                     ServiceLifeDiffDay = DateFormat.ToDiffDays(a.ServiceLife, DateTime.Now),
                 });
 
-                var notices = tmp.Where(a => a.ServiceLifeDiffDay <= AppConfig.ValidDay).ToList();
+                //待警示藥劑(母體)
+                var datas = tmp.Where(a => a.ServiceLifeDiffDay <= AppConfig.ValidDay).ToList();
+
+                //待警示藥劑單位
+                var units = datas.Select(a => new { a.City, a.Town }).Distinct();
+
+                //鄉鎮帳號
+                var accounts = new UsersService().GetAll().Where(a => a.MainContacter == "是").ToList();
+
+                foreach (var unit in units)
+                {
+                    var infos = datas.Where(a => a.City == unit.City && a.Town == unit.Town);
+                    var account = accounts.Where(a => a.City == unit.City && a.Town == unit.Town).FirstOrDefault();
+                    
+                    //紀錄查無主要聯絡人資訊
+                    if (account == null)
+                    {                        
+                        var msgs = infos.Select((a, index) => (index + 1).ToString() + "." + "藥劑(" + a.DrugName + ")：效期(" + a.ServiceLife + "),天數(" + a.ServiceLifeDiffDay + ")");
+                        string msg = string.Join("\r\n", msgs);
+
+                        string errors = string.Format("\r***查無此單位主要聯絡人：{0}{1}***\r{2}", 
+                                                unit.City, unit.Town, 
+                                                msg);
+                        logger.Error(errors);
+                        continue;
+                    }
+                    else
+                    {
+                        string aa = "!@3";
+                    }
+                    
+                    
+                }
 
                 return true;
             }
