cabaee2182ac2809e94905ae69d9306ae20e53ef
brianlin12345 <brianlin12345@gamil.com>
2024-11-01 13:14:43 +0800

    v2

---
+++ EPASchedule/ProCode/CtPointNotice.cs
@@ -0,0 +1,235 @@
+﻿using MyEPA.Models;
+using MyEPA.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EPASchedule
+{
+    internal class CtPointNotice
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public void Execute()
+        {
+            try
+            {
+                if (!Do())
+                {
+                    logger.Error("執行失敗");
+                }
+                else
+                {
+                    logger.Info("執行成功");
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error(ex.Message);
+                logger.Error(ex.StackTrace);
+            }
+        }
+
+        private bool Do()
+        {
+            try
+            {
+                DisinfectantService DisinfectantService = new DisinfectantService();
+
+                //所有藥劑
+                var ants = DisinfectantService.GetAll();
+
+                //測試 xxxxxxxxxxxxxxx
+                ants = ants.Where(a => a.City == "宜蘭縣").ToList();
+                //xxxxxxxxxxxxxxx
+
+                //City, Town, ContactUnit, DrugName
+                var tmp = ants.Select(a => new
+                {
+                    City = a.City,
+                    Town = a.Town,
+                    ContactUnit = a.ContactUnit,
+                    DrugName = a.DrugName,
+                    Amount = a.Amount,
+                    ServiceLife = a.ServiceLife,
+                    ServiceLifeDiffDay = DateFormat.ToDiffDays(a.ServiceLife, DateTime.Now),
+                });
+
+                //待警示藥劑(母體)
+                var datas = tmp.Where(a => a.ServiceLifeDiffDay <= AppConfig.ValidDay).ToList();
+
+                //待警示藥劑單位
+                var units = datas.Select(a => new { a.City, a.Town, a.ContactUnit }).Distinct().OrderBy(a => a.City);
+
+                //鄉鎮帳號
+                var accounts = new UsersService().GetAll().Where(a => a.MainContacter == "是").ToList();
+
+                //通知
+                foreach (var unit in units)
+                {
+                    var infos = datas.Where(a => a.City == unit.City && a.Town == unit.Town && a.ContactUnit == unit.ContactUnit);
+                    var account = accounts.Where(a => a.City == unit.City && a.Town == unit.Town).FirstOrDefault();
+
+                    //紀錄查無主要聯絡人資訊
+                    if (account == null)
+                    {
+                        var msgs = infos.Select((a, index) => (index + 1).ToString() + "." + "藥劑(" + a.DrugName + ")：效期(" + DateFormat.ToDate14(a.ServiceLife) + "),天數(" + a.ServiceLifeDiffDay + ")");
+                        string msg = string.Join("\r\n", msgs);
+
+                        string errors = string.Format("\r***無法通知，因查無此單位主要聯絡人：{0}{1}***\r{2}",
+                                                unit.City, unit.Town, unit.ContactUnit,
+                                                msg);
+                        logger.Error(errors);
+                        continue;
+                    }
+                    else
+                    {
+                        //寄發Mail
+                        //infos 資訊 + account 收件者帳號
+                        var aaa = infos.ToList();
+
+                        string msg = "";
+
+                        msg = @"
+<table border='1' Cellpadding='3' Cellspacing='3' width='40%'>
+     <tr>
+        <th width='10%'>項次</th>
+        <th width='30%'>消毒藥劑</th>
+        <th width='20%'>數量</th>
+        <th width='20%'>到期日</th>
+        <th width='20%'>剩餘天數</th>
+    </tr>";
+
+                        int index = 0;
+                        foreach (var info in infos)
+                        {
+                            index++;
+
+                            //超過期限
+                            string alertStyle = info.ServiceLifeDiffDay < 0 ? "style='color:red'" : "";
+
+                            msg = msg + string.Format(@"   
+    <tr>
+        <td align='center'>{0}</td>
+        <td align='center'>{1}</td>
+        <td align='center'>{2}</td>
+        <td align='center'>{3}</td>
+        <td align='center' {5}>{4}</td>
+    </tr>
+
+", index, info.DrugName, info.Amount, DateFormat.ToDate14(info.ServiceLife), info.ServiceLifeDiffDay, alertStyle);
+                        }
+
+                        msg = msg + @"
+</table>";
+
+                        string content = string.Format(@"
+
+{0}{1}，聯繫單位名稱({2})，{3}您好：
+<br/><br/>
+
+貴局尚有消毒藥劑使用期限即將到期，<br/>
+請優先使用以下藥劑以避免逾期藥效失效。
+<br/><br/>
+
+{4}
+
+",
+unit.City,
+unit.Town,
+unit.ContactUnit,
+account.Name,
+msg);
+
+                        bool done = ToSend(content, account);
+
+                        ////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        //if (done)
+                        //{
+                        //    logger.Info("寄信成功，開發階段只送一封");
+                        //    return true;
+                        //}
+                        ////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
+                    }
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.Error("執行錯誤 - Do");
+                logger.Error(ex.Message);
+                logger.Error(ex.StackTrace);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool ToSend(string content, UsersModel account)
+        {
+            bool result = false;
+
+            try
+            {
+                EmailHelper emailHelper = new EmailHelper();
+                MailParam p = new MailParam();
+                p.iniParam();
+                emailHelper.MailFrom = p.MailFrom;
+                emailHelper.MailFromName = p.MailFromName;
+                emailHelper.Account = p.Account;
+                emailHelper.Password = p.Password;
+                emailHelper.MailServer = p.MailServer;
+                emailHelper.MailPort = p.MailPort;
+                emailHelper.EnableSSL = p.EnableSSL;
+
+                emailHelper.Subject = "資源預警通報機制 - 閥值消毒設備藥劑到期通知";
+                emailHelper.Body = content;
+
+                //收件者
+                string addr1 = AppConfig.TestEmailAddress != "" ? AppConfig.TestEmailAddress : account.Email;
+                //addr1 = "123";  //xxxxxxxxxxxxxxxxxxxxxxxxx
+                emailHelper.AddTo(addr1, account.Name);
+
+                foreach (string addr in AppConfig.EmailAddressResp.Split(','))
+                {
+                    if (addr != "")
+                    {
+                        emailHelper.AddTo(addr, "");
+                    }
+                }
+
+                foreach (string addr in AppConfig.EmailAddressCC.Split(','))
+                {
+                    if (addr != "")
+                    {
+                        emailHelper.AddCC(addr, "");
+                    }
+                }
+
+                emailHelper.IsSendEmail = true;
+                bool success = emailHelper.SendBySmtp();
+
+                if (!success)
+                {
+                    logger.Error("ToSend - 信件寄發失敗:" + emailHelper.ToMails);
+                }
+
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                logger.Error("信件寄發錯誤：" + ex.Message);
+                logger.Error(ex.StackTrace);
+
+                return false;
+            }
+
+            return result;
+        }
+    }
+}
