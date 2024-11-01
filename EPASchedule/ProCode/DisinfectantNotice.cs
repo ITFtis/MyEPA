dce40546f25a6df04f56bd0bff31d74c5052b778
brianlin12345 <brianlin12345@gamil.com>
2024-11-01 10:47:12 +0800

    v1

--- EPASchedule/ProCode/DisinfectantNotice.cs
+++ EPASchedule/ProCode/DisinfectantNotice.cs
@@ -44,6 +44,10 @@ namespace EPASchedule
                 //所有藥劑
                 var ants = DisinfectantService.GetAll();
 
+                //測試 xxxxxxxxxxxxxxx
+                ants = ants.Where(a => a.City == "宜蘭縣").ToList();
+                //xxxxxxxxxxxxxxx
+
                 //City, Town, ContactUnit, DrugName
                 var tmp = ants.Select(a => new
                 {
@@ -51,6 +55,7 @@ namespace EPASchedule
                     Town = a.Town,
                     ContactUnit = a.ContactUnit,
                     DrugName = a.DrugName,
+                    Amount = a.Amount,
                     ServiceLife = a.ServiceLife,
                     ServiceLifeDiffDay = DateFormat.ToDiffDays(a.ServiceLife, DateTime.Now),
                 });
@@ -59,7 +64,7 @@ namespace EPASchedule
                 var datas = tmp.Where(a => a.ServiceLifeDiffDay <= AppConfig.ValidDay).ToList();
 
                 //待警示藥劑單位
-                var units = datas.Select(a => new { a.City, a.Town }).Distinct().OrderBy(a => a.City);
+                var units = datas.Select(a => new { a.City, a.Town, a.ContactUnit }).Distinct().OrderBy(a => a.City);
 
                 //鄉鎮帳號
                 var accounts = new UsersService().GetAll().Where(a => a.MainContacter == "是").ToList();
@@ -67,7 +72,7 @@ namespace EPASchedule
                 //通知
                 foreach (var unit in units)
                 {
-                    var infos = datas.Where(a => a.City == unit.City && a.Town == unit.Town);
+                    var infos = datas.Where(a => a.City == unit.City && a.Town == unit.Town && a.ContactUnit == unit.ContactUnit);
                     var account = accounts.Where(a => a.City == unit.City && a.Town == unit.Town).FirstOrDefault();
                     
                     //紀錄查無主要聯絡人資訊
@@ -77,7 +82,7 @@ namespace EPASchedule
                         string msg = string.Join("\r\n", msgs);
 
                         string errors = string.Format("\r***無法通知，因查無此單位主要聯絡人：{0}{1}***\r{2}", 
-                                                unit.City, unit.Town, 
+                                                unit.City, unit.Town, unit.ContactUnit,
                                                 msg);
                         logger.Error(errors);
                         continue;
@@ -86,26 +91,71 @@ namespace EPASchedule
                     {
                         //寄發Mail
                         //infos 資訊 + account 收件者帳號
-                        var msgs = infos.Select((a, index) => (index + 1).ToString() + "." + a.DrugName + "到期日(" + DateFormat.ToDate14(a.ServiceLife) + ")，剩餘天數(" + a.ServiceLifeDiffDay + ")");
-                        string msg = string.Join("\r\n", msgs);
+                        var aaa = infos.ToList();
+
+                        string msg = "";
+
+                        msg = @"
+<table border='1' Cellpadding='3' Cellspacing='3' width='40%'>
+     <tr>
+        <th width='10%'>項次</th>
+        <th width='30%'>消毒藥劑</th>
+        <th width='20%'>數量</th>
+        <th width='20%'>到期日</th>
+        <th width='20%'>剩餘天數</th>
+    </tr>";
+
+                        int index = 0;
+                        foreach (var info in infos)
+                        {
+                            index++;
+
+                            //超過期限
+                            string alertStyle = info.ServiceLifeDiffDay < 0 ? "style='color:red'" : "";
+
+                            msg = msg + string.Format(@"   
+    <tr>
+        <td align='center'>{0}</td>
+        <td align='center'>{1}</td>
+        <td align='center'>{2}</td>
+        <td align='center'>{3}</td>
+        <td align='center' {5}>{4}</td>
+    </tr>
+
+", index, info.DrugName, info.Amount, DateFormat.ToDate14(info.ServiceLife), info.ServiceLifeDiffDay, alertStyle);
+                        }
+
+                        msg = msg + @"
+</table>";
 
                         string content = string.Format(@"
 
-{0}清潔隊您好，
+{0}{1}，聯繫單位名稱({2})，{3}您好：
 <br/><br/>
 
 貴局尚有消毒藥劑使用期限即將到期，<br/>
 請優先使用以下藥劑以避免逾期藥效失效。
 <br/><br/>
 
-{1}
+{4}
 
 ",
-unit.City, unit.Town,
-msg
-);
+unit.City, 
+unit.Town,
+unit.ContactUnit,
+account.Name,
+msg);
 
-                        bool done = ToSend(msg, account);
+                        bool done = ToSend(content, account);
+
+                        ////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+                        //if (done)
+                        //{
+                        //    logger.Info("寄信成功，開發階段只送一封");
+                        //    return true;
+                        //}
+                        ////xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+
                     }
                 }
 
