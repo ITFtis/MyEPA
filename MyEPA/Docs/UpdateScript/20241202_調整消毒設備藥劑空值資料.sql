57c1de895c959dbc53df6f193a77a7803bb83ce2
brianlin12345 <brianlin12345@gamil.com>
2024-12-02 09:06:14 +0800

    script

---
+++ MyEPA/Docs/UpdateScript/20241202_調整消毒設備藥劑空值資料.sql
@@ -0,0 +1,12 @@
+--調整消毒設備藥劑空值資料
+--Select IsSupportCity, SupportCityNum From Disinfector Where IsSupportCity Is Null
+--Select IsSupportCity, SupportCityNum From Disinfectant  Where IsSupportCity Is Null
+
+Update Disinfector
+Set IsSupportCity = 0
+Where IsSupportCity Is Null
+
+Update Disinfectant
+Set IsSupportCity = 0
+Where IsSupportCity Is Null
+
