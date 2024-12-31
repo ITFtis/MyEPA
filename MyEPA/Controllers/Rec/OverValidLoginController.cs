bb4228059bcf9520fac4a82a5a711f57245c3c3f
brianlin12345 <brianlin12345@gamil.com>
2024-12-31 16:12:43 +0800

    聯絡人未登入三個月 v1

---
+++ MyEPA/Controllers/Rec/OverValidLoginController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEPA.Controllers.Rec
+{
+    public class OverValidLoginController : LoginBaseController
+    {
+        // GET: OverValidLogin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
