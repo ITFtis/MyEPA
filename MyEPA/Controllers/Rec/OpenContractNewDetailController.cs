380ea17f354398a2ba1c23cd0b1ca558baff1f97
brianlin12345 <brianlin12345@gamil.com>
2025-02-24 11:03:16 +0800

    新版開口合約 d1 細目清單

---
+++ MyEPA/Controllers/Rec/OpenContractNewDetailController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using MyEPA.Enums;
+using MyEPA.Extensions;
+using MyEPA.Helper;
+using MyEPA.Models;
+using MyEPA.Services;
+using MyEPA.ViewModels;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyEPA.Controllers.Rec
+{
+    public class OpenContractNewDetailController : LoginBaseController
+    {
+        OpenContractService OpenContractService = new OpenContractService();
+        OpenContractDetailService OpenContractDetailService = new OpenContractDetailService();
+
+        // GET: OpenContractNewDetail
+        public ActionResult Index(int openContractId)
+        {
+            ViewBag.OpenContract = OpenContractService.Get(openContractId);
+
+            var result = OpenContractDetailService.GetList(openContractId);
+
+            return View(result);
+        }
+    }
+}
