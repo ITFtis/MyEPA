bbe06633d88a3286d1b6b6ba9adcfc4f99a1974e
brianlin12345 <brianlin12345@gamil.com>
2025-03-05 15:05:11 +0800

    檔案格式驗證 accept v1

---
+++ MyEPA/Scripts/baseb.js
@@ -0,0 +1,19 @@
+﻿//限定副檔名(accept) 挑選檔案有在accept
+function InputFileAccept(objInput) {
+
+    var result = false;
+
+    var exts = $(objInput).attr('accept').split(',');
+    var fName = objInput.files[0].name;
+    var fExt = fName.substr(fName.lastIndexOf('.'), fName.length);
+
+    $(exts).each(function () {
+        var ext = this.trim();
+        if (fExt.toLowerCase() == ext.toLowerCase()) {
+            result = true;
+            return false;
+        }
+    });
+
+    return result;
+}
